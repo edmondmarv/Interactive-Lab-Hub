dcd82e3281b6d0aefad3f830327737c1b4e6cb3f
Marvin Edmond <marvin.edmond@rutgers.edu>
2021-12-20 00:39:57 -0500

    Add files via upload

---
+++ Lab 3/t2s_all_steps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
+#say $*
+say " Hi Marvin! Please tell me more about your new t-shirt."
+
+#!/bin/bash
+say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
+#say $*
+say " New T-shirt. Color is black. Size is large. Fabric is cotton. Please insert when you're ready. Thank you."
